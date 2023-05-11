ba6f4e5787ba05024123d63aa4afe0a8b4622856
giuvan <giuvanreginatto@hotmail.com>
2023-05-10 23:58:29 -0300

    teste1

--- examples/t1/test/TestSort.c
+++ examples/t1/test/TestSort.c
@@ -15,8 +15,8 @@ TEST_TEAR_DOWN(Sort)
 
 TEST(Sort, LimiteInferiorDentro)
 {
-  int b[] = {22, 17};
-  int b1[] = {17, 22};
-  TEST_ASSERT_EQUAL(0, sort(b, 2, (char *)"On", COUNTING));
-  //TEST_ASSERT_EQUAL_INT32_ARRAY(b1, b, 2);
+    int b[] = {22, 17};
+    int b1[] = {17, 22};
+    TEST_ASSERT_EQUAL(0, sort(b, 2, (char *)"On", COUNTING));
+    TEST_ASSERT_EQUAL_INT32_ARRAY(b1, b, 2);
 }
