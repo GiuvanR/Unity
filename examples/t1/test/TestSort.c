6703ceea47a43a6e9acea46763be9c93d156aa7f
giuvan <giuvanreginatto@hotmail.com>
2023-05-11 13:52:23 -0300

    teste

--- examples/t1/test/TestSort.c
+++ examples/t1/test/TestSort.c
@@ -57,3 +57,108 @@ TEST(Sort, TypeErradoCounting)
   int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
   TEST_ASSERT_EQUAL(1, sort(a, 10, (char *)"On2", COUNTING));
 }
+
+TEST(Sort, TestRadixCorreto)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  int a1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  TEST_ASSERT_EQUAL(0, sort(a, 10, (char *)"On", RADIX));
+  TEST_ASSERT_EQUAL_INT32_ARRAY(a1, a, 10);
+}
+
+TEST(Sort, TypeErradoRadix)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  TEST_ASSERT_EQUAL(1, sort(a, 10, (char *)"Onlogn", RADIX));
+}
+
+TEST(Sort, TestBubbleCorreto)
+{
+  int d[] = {7, 8, 20, 3, 19, 15, 17, 2, 6, 384, 495, 4, 1, 1000, 8945, 5, 0, 1968, 15000, 9};
+  int d1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 17, 19, 20, 384, 495, 1000, 1968, 8945, 15000};
+  TEST_ASSERT_EQUAL(0, sort(d, 20, (char *)"On2", BUBBLE));
+  TEST_ASSERT_EQUAL_INT32_ARRAY(d1, d, 20);
+}
+
+TEST(Sort, TypeErradoBubble)
+{
+  int d[] = {7, 8, 20, 3, 19, 15, 17, 2, 6, 384, 495, 4, 1, 1000, 8945, 5, 0, 1968, 15000, 9};
+  TEST_ASSERT_EQUAL(1, sort(d, 20, (char *)"On", BUBBLE));
+}
+
+
+TEST(Sort, TestInsertionCorreto)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  int a1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  TEST_ASSERT_EQUAL(0, sort(a, 10, (char *)"On2", INSERTION));
+  TEST_ASSERT_EQUAL_INT32_ARRAY(a1, a, 10);
+}
+
+TEST(Sort, TypeErradoInsertion)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  TEST_ASSERT_EQUAL(1, sort(a, 10, (char *)"Onlogn", INSERTION));
+}
+
+TEST(Sort, TestSelectionCorreto)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  int a1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  TEST_ASSERT_EQUAL(0, sort(a, 10, (char *)"On2", SELECTION));
+  TEST_ASSERT_EQUAL_INT32_ARRAY(a1, a, 10);
+}
+
+TEST(Sort, TypeErradoSelection)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  TEST_ASSERT_EQUAL(1, sort(a, 10, (char *)"On", SELECTION));
+}
+
+TEST(Sort, TestHeapCorreto)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  int a1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  TEST_ASSERT_EQUAL(0, sort(a, 10, (char *)"Onlogn", HEAP));
+  TEST_ASSERT_EQUAL_INT32_ARRAY(a1, a, 10);
+}
+
+TEST(Sort, TypeErradoHeap)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  TEST_ASSERT_EQUAL(1, sort(a, 10, (char *)"On", HEAP));
+}
+
+TEST(Sort, TestMergeCorreto)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  int a1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  TEST_ASSERT_EQUAL(0, sort(a, 10, (char *)"Onlogn", MERGE));
+  TEST_ASSERT_EQUAL_INT32_ARRAY(a1, a, 10);
+}
+
+TEST(Sort, TypeErradoMerge)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  TEST_ASSERT_EQUAL(1, sort(a, 10, (char *)"On2", MERGE));
+}
+
+TEST(Sort, TestQuickCorreto)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  int a1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  TEST_ASSERT_EQUAL(0, sort(a, 10, (char *)"Onlogn", QUICK));
+  TEST_ASSERT_EQUAL_INT32_ARRAY(a1, a, 10);
+}
+
+TEST(Sort, TypeErradoQuick)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  TEST_ASSERT_EQUAL(1, sort(a, 10, (char *)"On", QUICK));
+}
+
+TEST(Sort, TestDefault)
+{
+  int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  TEST_ASSERT_EQUAL(1, sort(a, 10, (char *)"On", 9));
+}
--- examples/t1/test/test_runners/TestSort_Runner.c
+++ examples/t1/test/test_runners/TestSort_Runner.c
@@ -9,4 +9,19 @@ TEST_GROUP_RUNNER(Sort)
   RUN_TEST_CASE(Sort, LimiteSuperiorFora);
   RUN_TEST_CASE(Sort, ValorIntermediarioLength);
   RUN_TEST_CASE(Sort, TypeErradoCounting);
+  RUN_TEST_CASE(Sort, TestRadixCorreto);
+  RUN_TEST_CASE(Sort, TypeErradoRadix);
+  RUN_TEST_CASE(Sort, TestBubbleCorreto);
+  RUN_TEST_CASE(Sort, TypeErradoBubble);
+  RUN_TEST_CASE(Sort, TestInsertionCorreto);
+  RUN_TEST_CASE(Sort, TypeErradoInsertion);
+  RUN_TEST_CASE(Sort, TestSelectionCorreto);
+  RUN_TEST_CASE(Sort, TypeErradoSelection);
+  RUN_TEST_CASE(Sort, TestHeapCorreto);
+  RUN_TEST_CASE(Sort, TypeErradoHeap);
+  RUN_TEST_CASE(Sort, TestMergeCorreto);
+  RUN_TEST_CASE(Sort, TypeErradoMerge);
+  RUN_TEST_CASE(Sort, TestQuickCorreto);
+  RUN_TEST_CASE(Sort, TypeErradoQuick);
+  RUN_TEST_CASE(Sort, TestDefault);
 }
