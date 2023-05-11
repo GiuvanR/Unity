b77656b291e0321fadd08e1f9a40643fc9abaa4b
giuvan <giuvanreginatto@hotmail.com>
2023-05-10 22:21:24 -0300

    teste1

---
+++ examples/t1/test/TestSort.c
@@ -0,0 +1,22 @@
+#include "src/sort.h"
+#include "unity.h"
+#include "unity_fixture.h"
+
+
+TEST_GROUP(Sort);
+
+TEST_SETUP(Sort)
+{
+}
+
+TEST_TEAR_DOWN(Sort)
+{
+}
+
+TEST(Sort, LimiteInferiorDentro)
+{
+  int b[] = {22, 17};
+  int b1[] = {17, 22};
+  TEST_ASSERT_EQUAL(0, sort(b, 2, (char *)"On", COUNTING));
+  TEST_ASSERT_EQUAL_INT32_ARRAY(b1, b, 2);
+}
